8af960429ee2dfe35f17f36990910c2c21ac01f2
Ose-Augustine <osebeckley@gmail.com>
2023-06-24 18:45:12 +0100

    Mapping loaded textures.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import { motion } from "framer-motion"
 import { Canvas } from "@react-three/fiber"
 import { useLoader } from "@react-three/fiber"
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
-import { useEffect } from "react"
+import { TextureLoader } from "three/src/loaders/TextureLoader"
 
 export default function Hero() {
+    const [colorMap, roughnessMap] = useLoader(TextureLoader, [
+        'public/textures/coast_sand_rocks_02_diff_4k.jpg',
+        'public/textures/coast_sand_rocks_02_disp_4k.png',
+    ])
     return (
         <motion.article
             className="bg-[#212121] h-screen text-white  flex justify-evenly w-screen"
@@ -18,11 +21,19 @@ export default function Hero() {
             </section>
             <section className="w-1/2">
                 <Canvas>
-                    <ambientLight intensity={0.1} />
-                    <directionalLight color='red' position={[0,0,5]}/>
+                    <hemisphereLight 
+                        skyColor={'#ffffff'}
+                        groundColor={'#212121'}
+                        intensity= {0.8}
+                    />
+                    {/* <directionalLight color='red' position={[0,0,5]}/> */}
                     <mesh>
                         <sphereGeometry />
-                        <meshStandardMaterial />
+                        <meshStandardMaterial 
+                            map={colorMap}
+                            bumpMap={colorMap}
+                            //displacementMap={colorMap}
+                        />
                     </mesh>
                 </Canvas>
             </section>
