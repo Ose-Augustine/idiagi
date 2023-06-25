0c46a099d7d9a4bb70d521e5a79fdffdf0bda401
Ose-Augustine <osebeckley@gmail.com>
2023-06-25 23:05:19 +0100

    Creating staggered animation using variants

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-
+import { container, items } from "../components/animation";
 export default function About() {
      return (
         <motion.div className="h-screen  bg-cover bg-left"
@@ -7,6 +7,14 @@ export default function About() {
         style={{backgroundImage: "url('/skyscraper.jpg')"}}
         animate = {{opacity:1}}
         transition={{duration:0.75 , ease: "easeIn"}}
-        ></motion.div>
+        >
+            <motion.div variants={container} initial="hidden" animate="show">
+                <motion.div className="overflow-hidden" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, sapiente.</motion.div>
+                <motion.div className="overflow-hidden" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veritatis.</motion.div >
+                <motion.div className="overflow-hidden" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ex?</motion.div>
+                <motion.div className="overflow-hidden" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, recusandae.</motion.div>
+            </motion.div>
+
+        </motion.div>
      )
 }
