ee52b0f06d0aeec5cf0173f96c169eb20350446e
Ose-Augustine <osebeckley@gmail.com>
2023-06-26 08:41:01 +0100

    Slide containers moving out of the tower

--- src/components/animation.js
+++ src/components/animation.js
@@ -9,12 +9,36 @@ export const container = {
     }, 
 };
 
-export const items = {
-    hidden: {opacity:0}, 
+export const itemsLeft = {
+    hidden: {
+        x: '100%',
+        opacity:0, 
+        transition:{
+            duration:0.1
+        }
+    }, 
     show: {
-        opacity:1, 
+        x: '0%',
+        opacity:1,
         transition: {
-            duration: 0.5,
+            duration: 2,
+        }
+    }
+};
+
+export const itemsRight = {
+    hidden: {
+        x: '-100%',
+        opacity:0, 
+        transition:{
+            duration:0.1
+        }
+    }, 
+    show: {
+        x: '0%',
+        opacity:1,
+        transition: {
+            duration: 2,
         }
     }
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { container, items } from "../components/animation";
+import { container, itemsLeft, itemsRight } from "../components/animation";
 export default function About() {
      return (
         <motion.div className="h-screen  bg-cover bg-left flex flex-col justify-center lg:items-center"
@@ -8,11 +8,23 @@ export default function About() {
         animate = {{opacity:1}}
         transition={{duration:0.75 , ease: "easeIn"}}
         >
-            <motion.div className="border-4 border-red-500 h-3/4 relative text-white text-lg grid grid-cols-2 lg:w-1/2 gap-36 lg:gap-x-72"variants={container} initial="hidden" animate="show">
-                <motion.div className="overflow-hidden border-green-400 border-4" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veritatis.</motion.div>
-                <motion.div className="overflow-hidden  border-green-500 border-4" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veritatis.</motion.div >
-                <motion.div className="overflow-hidden" variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ex?</motion.div>
-                <motion.div className="overflow-hidden " variants={items}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, recusandae.</motion.div>
+            <motion.div className=" h-3/4 relative text-white text-lg grid grid-cols-2 lg:w-1/2 gap-36 lg:gap-x-72"variants={container} initial="hidden" animate="show">
+                <motion.div className="overflow-hidden " variants={itemsLeft}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veritatis. 
+                    this is the name of the man since no lorem was found
+                </motion.div>
+                <motion.div className="overflow-hidden" variants={itemsRight}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, veritatis.
+                    this is the name of the man since no lorem was found
+                </motion.div >
+                <motion.div className="overflow-hidden" variants={itemsLeft}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ex? 
+                    this is the name of the man since no lorem was found
+                </motion.div>
+                <motion.div className="overflow-hidden " variants={itemsRight} >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, recusandae. 
+                    this is the name of the man since no lorem was found
+                </motion.div>
             </motion.div>
 
         </motion.div>
