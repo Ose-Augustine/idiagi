8a0c61a7c4115b447d088562adc9d5d6e5a0fb7d
Ose-Augustine <osebeckley@gmail.com>
2023-06-27 07:07:38 +0100

    Staggered animation on indirect children

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,17 +11,17 @@ export default function Body() {
             variants= {container} initial="hidden" animate="show"
             style={{backgroundImage: "url('/bloodmoon.jpg')"}}
             className="h-screen border-2 border-black text-white bg-center bg-cover">
-            <div className="flex w-full h-1/3 border-red-500 border-4">
-                <img className="w-1/2 object-cover"src="/forestpath.jpg" alt="" />
-                <p className="p-14 w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repellendus ea voluptates ex amet illo.</p>
+            <div className="flex w-full h-1/3 border-orange-200 border-dotted border-2 mb-20">
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover"src="/forestpath.jpg" alt="" />
+                <motion.p variants={itemsRight} className="p-14 w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repellendus ea voluptates ex amet illo.</motion.p>
             </div>
             <div className="flex w-full h-1/3">
-                <p className="p-14">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In animi non architecto odio officiis? Sapiente?</p>
-                <img className="w-1/2 object-cover" src="/shrubsunset.jpg" alt="" />
+                <motion.p variants={itemsRight} className="p-14">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In animi non architecto odio officiis? Sapiente?</motion.p>
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover" src="/shrubsunset.jpg" alt="" />
             </div>
             <div className="flex w-full h-1/3">
-                <img className="w-1/2 object-cover"src="/waterfall.jpg" alt="" />
-                <p className="p-14 w-1/2 border-green-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem exercitationem modi pariatur ex. Asperiores?</p>
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover"src="/waterfall.jpg" alt="" />
+                <motion.p variants={itemsRight} className="p-14 w-1/2 border-green-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem exercitationem modi pariatur ex. Asperiores?</motion.p>
             </div>
         </motion.section>
      
