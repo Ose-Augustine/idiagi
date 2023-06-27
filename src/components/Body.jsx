37ad61af321a8b9596cd3e740f96fb0db6749e45
Ose-Augustine <osebeckley@gmail.com>
2023-06-27 08:54:24 +0100

    Responsive to large screens

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -21,18 +21,20 @@ export default function Body() {
             ref={ref} 
             variants= {container} initial="hidden" animate={mainControls}
             style={{backgroundImage: "url('/bloodmoon.jpg')"}}
-            className="border-2 h-screen border-black text-white bg-center bg-cover ">
-            <div className="flex w-full h-1/3 border-orange-200 border-dotted border-2">
-                <motion.img variants={itemsLeft} className="w-1/2 object-cover"src="/forestpath.jpg" alt="" />
-                <motion.p variants={itemsRight} className="p-14 w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repellendus ea voluptates ex amet illo.</motion.p>
+            className="border-2  border-black text-white bg-center bg-cover ">
+            <div className="h-full lg:m-16 lg:text-3xl ">
+            <div className="flex w-full h-96  lg:my-24">
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover border-2 border-orange-200 rounded-xl"src="/forestpath.jpg" alt="" />
+                <motion.p variants={itemsRight} className="p-14 w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repellendus ea voluptates ex amet illo. the name of the man was added to this</motion.p>
             </div>
-            <div className="flex w-full h-1/3">
-                <motion.p variants={itemsRight} className="p-14">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In animi non architecto odio officiis? Sapiente?</motion.p>
-                <motion.img variants={itemsLeft} className="w-1/2 object-cover" src="/shrubsunset.jpg" alt="" />
+            <div className="flex w-full h-96 lg:my-24">
+                <motion.p variants={itemsRight} className="p-14 ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In animi non architecto odio officiis? Sapiente?</motion.p>
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover border-2 border-orange-200 rounded-xl" src="/shrubsunset.jpg" alt="" />
             </div>
-            <div className="flex w-full h-1/3">
-                <motion.img variants={itemsLeft} className="w-1/2 object-cover"src="/waterfall.jpg" alt="" />
+            <div className="flex w-full h-96 lg:my-24">
+                <motion.img variants={itemsLeft} className="w-1/2 object-cover border-2 border-orange-200 rounded-lg"src="/waterfall.jpg" alt="" />
                 <motion.p variants={itemsRight} className="p-14 w-1/2 border-green-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem exercitationem modi pariatur ex. Asperiores?</motion.p>
+            </div>
             </div>
         </motion.section>
      
