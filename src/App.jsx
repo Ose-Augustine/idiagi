29483f6fb9080d7d140f44cd4138ec88da2f4085
Ose-Augustine <osebeckley@gmail.com>
2023-06-27 14:04:52 +0100

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { motion } from "framer-motion"
 import Hero from "./components/Hero"
 import About from "./pages/About"
 import Body from "./components/Body"
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 function App() {
   
   return (
    <>
       <Router>
+        <nav className="absolute z-20 text-white flex justify-evenly w-full text-lg">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/body">Body</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/about" element={<About />} />
